f4f55624ab3add516486423a078e688d5637c96b
Vikash Kushwaha <136932881+Kushwaha2406Vikash@users.noreply.github.com>
2025-08-24 23:02:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // ✅ Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173", // 👈 env based frontend URL
+  origin: process.env.CLIENT_URL || "https://constitution-ai-rag-iyj4.vercel.app/", /
   methods: ["GET", "POST"],
   credentials: true,
 }));
